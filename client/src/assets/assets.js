8942a25729a79ef259ad7d7e31ef617c7db6de7a
rushi0212 <152675340+rushi0212@users.noreply.github.com>
2025-08-03 18:25:19 +0530

    Add files via upload

---
+++ client/src/assets/assets.js
@@ -0,0 +1,38 @@
+import email_icon from './email_icon.svg'
+import lock_icon from './lock_icon.svg'
+import cross_icon from './cross_icon.svg'
+import star_group from './star_group.png'
+import credit_star from './credit_star.svg'
+import profile_icon from './profile_icon.png'
+import dog from './dog.jpg'
+
+export const assets = {
+    email_icon,
+    lock_icon,
+    cross_icon,
+    star_group,
+    credit_star,
+    profile_icon,
+    dog
+}
+
+export const plans = [
+    {
+      id: 'Basic',
+      price: 10,
+      credits: 100,
+      desc: 'Best for personal use.'
+    },
+    {
+      id: 'Advanced',
+      price: 50,
+      credits: 500,
+      desc: 'Best for business use.'
+    },
+    {
+      id: 'Business',
+      price: 250,
+      credits: 5000,
+      desc: 'Best for enterprise use.'
+    },
+  ]
